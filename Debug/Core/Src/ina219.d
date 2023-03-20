c11535fa69d4338fffd65e08d733ec115d53946f
motorrr4ik <nikita29042001@yandex.ru>
2023-03-20 12:52:11 +0300

    update

--- Debug/Core/Src/ina219.d
+++ Debug/Core/Src/ina219.d
@@ -22,7 +22,10 @@ Core/Src/ina219.o: ../Core/Src/ina219.c ../Core/Inc/ina219.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_i2c.h \
- ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_uart.h
 ../Core/Inc/ina219.h:
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32F1xx_hal.h:
 ../Core/Inc/stm32f1xx_hal_conf.h:
@@ -48,3 +51,6 @@ Core/Src/ina219.o: ../Core/Src/ina219.c ../Core/Inc/ina219.h \
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h:
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_i2c.h:
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_uart.h:
